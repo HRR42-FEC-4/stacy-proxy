1511934e349a1ce5e9ca3ae2af2892d5be5e56e1
Stacy Kim <stacy.kim@ucla.edu>
2019-12-15 19:02:54 -0600

    Change stacy-service to run on single port 3002

--- stacy-service/server/index.js
+++ stacy-service/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Artist, Artwork, db } = require('./models');
 const routes = require('./routes');
 
-const port = 3500;
+const port = 3002;
 
 const app = express();
 
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', routes);
+
+app.use('/', express.static('./public'));
 
 ( async () => {
   try {
